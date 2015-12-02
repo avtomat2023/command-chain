d656a8897d69af0e807cf083b362c01575d70254
Yusuke Yoshimoto <immortal836@gmail.com>
2015-12-02 13:56:13 +0900

    Add preamble

--- command-chain.el
+++ command-chain.el
@@ -1,4 +1,26 @@
-;;; Word Chain game by emacs command names.
+;;; command-chain.el --- Word Chain Game by Emacs Command Names
+
+;; Copyright (C) 2015 Yusuke Yoshimoto
+;; Version: 20151202
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; See README on https://github.com/yoshrc/command-chain
+
+;;; Code:
 
 (require 'widget)
 
